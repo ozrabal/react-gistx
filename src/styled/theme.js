b30005c6df676fda4d9edb583f19ec0235281006
Piotr Łepkowski <ozrabal@gmail.com>
2017-11-09 00:03:37 +0100

    styled comp theme

---
+++ src/styled/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+    borderColor: '#dddddd',
+    borderRadius: '3px',
+    textColor: '#666666',
+    headerBg: '#f7f7f7',
+
+    tagBtnBorderColor: '#dddddd',
+    tagBtnBorderRadius: '3px',
+    tagBtnBg: '#f7f7f7',
+    tagBtnTextColor: '#666666',
+    tagBtnHoverTextColor: '#000000',
+    tagBtnHoverBg: '#ffffff',
+}
