e1b29f0ca525517403cff8644e92cee2f75f4a66
Piotr Łepkowski <ozrabal@gmail.com>
2017-11-09 23:50:31 +0100

    error boundary component added

---
+++ src/components/errorBoundary.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+/**
+ * Error Boundary component
+ * @extends Component
+ */
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true });
+    }
+
+    render() {
+        console.log('er', this.state)
+        if (this.state.hasError) {
+            return <h1>Something went wrong.</h1>;
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary
